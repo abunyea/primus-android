d44f8fa7e68289d47b9d2b4ba96842d1ec74ee29
Andrew Bunyea <abunyea@gmail.com>
2013-07-08 13:28:10 -0400

    Added elephants project

---
+++ elephants/Elephant.java
@@ -0,0 +1,60 @@
+package elephants;
+
+public class Elephant {
+
+	public void setMother(Elephant mother) {
+		
+	}
+	
+	public Elephant getMother() {
+		return null;
+	}
+	
+	public void setFather(Elephant father) {
+		
+	}
+	
+	public Elephant getFather() {
+		return null;
+	}
+	
+	public void setGender(char gender) {
+		
+	}
+	
+	public char getGender() {
+		return '0';
+	}
+	
+	public void setAge(int age) {
+		
+	}
+	
+	public int getAge() {
+		return 0;
+	}
+	
+	public void setName() {
+		
+	}
+	
+	public String getName() {
+		return null;
+	}
+	
+	@Override
+	public String toString() {
+		return null;
+	}
+	
+	@Override
+	public boolean equals(Object other) {
+		return false;
+	}
+	
+	@Override
+	public int hashCode() {
+		return 0;
+	}
+
+}
---
+++ elephants/ElephantTracker.java
@@ -0,0 +1,22 @@
+package elephants;
+
+public class ElephantTracker {
+
+	public static void main(String[] args) {
+		// Give the user some options,
+		// one of the options should be add elephant
+		// another should be to print all elephants
+		
+		// Use a loop to ask over and over
+	}
+	
+	public static Elephant inputElephant() {
+		// ask the user any input questions
+		// and parse their answers
+		
+		// use the elephant constructor
+		// and return statement
+		return null;
+	}
+
+}
